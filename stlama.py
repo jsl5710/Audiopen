a0733037022d12c11fba3564f4513149a9f5b1f6
prathapkr <142242597+prathapkr@users.noreply.github.com>
2023-09-05 12:36:54 +0530

    Update stlama.py

--- stlama.py
+++ stlama.py
@@ -1,31 +1,33 @@
+# Import necessary libraries and modules
 import re
-import os
-import time
-import streamlit as st
-import openai
-from langchain.chat_models import ChatOpenAI
-
+import config
 from langchain import PromptTemplate
-from st_custom_components import st_audiorec
 from langchain import LLMChain
 from langchain.llms import OpenAI
+import ffmpeg
+import openai
+import streamlit as st
+from st_custom_components import st_audiorec
 
 # Set OpenAI API key
 openai.api_key = st.secrets['OPENAI_API_KEY']
 
 # Set Streamlit page configuration
 st.set_page_config(page_title="streamlit_audio_recorder")
-st.markdown("""<style>.css-1egvi7u {margin-top: -3rem;}</style>""", unsafe_allow_html=True)
+st.markdown(
+    """<style>.css-1egvi7u {margin-top: -3rem;}</style>""", unsafe_allow_html=True
+)
 
 # Define Streamlit app header
-st.header(":violet[Audio]  :orange[Whisper] :headphones:")
+st.header(":violet[Audio]  :orange[Whisper] :headphones:", divider="violet")
 st.write(":violet[Unleash Ideas through Audio]")
 st.write("\n\n")
 
+
+# Function to transcribe audio and process text
 # Function to transcribe audio and process text
 @st.cache_data
 def process_audio(audio_file):
-    start_time = time.time()
     try:
         # Transcribe the audio
         transcript = openai.Audio.transcribe("whisper-1", audio_file)
@@ -33,32 +35,32 @@ def process_audio(audio_file):
 
         # LangChain template for text processing
         template = """
-        You have the unique ability to convert spoken ideas into coherent text. 
-        Imagine that you're assisting someone who has just recorded their thoughts in audio. 
-        These thoughts might be a bit unorganized or lengthy. Your task is to transform this spoken content into two key parts:
 
-        1. A concise headline: Capture the essence of the spoken ideas in a short, attention-grabbing sentence.
-        2. Clear text: Explain the main points or details in a straightforward, easy-to-understand paragraph.
+        You're an Expert in converting spoken ideas into coherent text. Imagine that you're assisting someone who has just recorded their thoughts in audio. These thoughts might be a bit unorganized or lengthy. Your task is to transform this spoken content, which is represented by the transcription below, into two key parts:
+
+1. Headline: Capture the essence of the spoken ideas in a short, attention-grabbing sentence.
+2. Clear text: Explain the main points or details in a straightforward, easy-to-understand paragraph.
+
+Transcription: {x}
+
+the output should consist of both the headline and the Clear text.
+        
         """
 
-        sprompt = PromptTemplate.from_template(template, x=x)
+        sprompt = PromptTemplate.from_template(template)
 
         # Initialize the models
-        llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=st.secrets['OPENAI_API_KEY'])
+        llm = OpenAI(model_name="gpt-3.5-turbo", openai_api_key=st.secrets['OPENAI_API_KEY'], temperature=0)
         llm_chain = LLMChain(prompt=sprompt, llm=llm)
 
         # Process the text
         z = llm_chain.run(x)
         headline_match = re.search(r"Headline:\s*(.*?)\n", z, re.DOTALL)
-        clear_text_match = re.search(r"Clear Text:\s*(.*?)$", z, re.DOTALL)
+        clear_text_match = re.search(r"Clear text:\s*(.*?)$", z, re.DOTALL)
 
         # Check if matches were found and extract the text
         headline = headline_match.group(1).strip() if headline_match else ""
         clear_text = clear_text_match.group(1).strip() if clear_text_match else ""
-
-        end_time = time.time()
-        execution_time = end_time - start_time
-        st.write("Execution time:", execution_time, "seconds")
 
         return headline, clear_text, transcript
 
@@ -66,73 +68,51 @@ def process_audio(audio_file):
         st.error("Error: The audio file is too short. Minimum audio length is 0.1 seconds.")
         return "", "", None
 
+
+# Streamlit app
 def audiorec_demo_app():
     # Record audio using the custom component
-    wav_audio_data1 = st.file_uploader("Record audio", type=["wav"])
-    wav_audio_data2 = st_audiorec()
+    wav_audio_data = st_audiorec()
 
     # Process audio data if available
-    if wav_audio_data2 is not None:
+    if wav_audio_data is not None:
         # Save the audio to a WAV file
         with open("recorded_audio.wav", "wb") as wav_file:
-            wav_file.write(wav_audio_data2)
-
-            # Transcribe and process the audio
-            file = "./recorded_audio.wav"
-            audio = open(file, "rb")
-        try:
-            with st.spinner("Processing Your Ideas..."):
-                headline, clear_text, transcript = process_audio(audio)
-
-            with st.expander("Original Transcription"):
-                st.write(transcript)
-
-            if headline and clear_text:
-                from streamlit_extras.colored_header import colored_header
-
-                colored_header(
-                    label=headline,
-                    description=clear_text,
-                    color_name="violet-70",
-                )
-        except Exception as e:
-            st.error(f"An error occurred while processing: {e}")
-
-    # Process audio data if available
-    if wav_audio_data1 is not None:
-        # Save the audio to a WAV file
-        upload_dir = 'uploads'
-        os.makedirs(upload_dir, exist_ok=True)
-        wav_file_path = os.path.join(upload_dir, "recorded_audio.wav")
-        with open(wav_file_path, "wb") as wav_file:
-            wav_file.write(wav_audio_data1.read())
+            wav_file.write(wav_audio_data)
 
         # Transcribe and process the audio
-        audio_file = open(wav_file_path, "rb")
+        file = "./recorded_audio.wav"
+        audio_file = open(file, "rb")
 
-        # Error handling for processing
-        try:
-            with st.spinner("Processing Your Ideas..."):
-                headline, clear_text, transcript = process_audio(audio_file)
+        with st.status(""":rainbow[Processing Your Ideas... ]"""):
+            headline, clear_text, transcript = process_audio(audio_file)
+        # st.info(headline)
+        # st.success(clear_text)
+        from streamlit_extras.colored_header import colored_header
 
-            with st.expander("Original Transcription"):
-                st.write(transcript)
+        with st.expander("Original Transcription"):
+            st.write(transcript)
 
-            if headline and clear_text:
-                from streamlit_extras.colored_header import colored_header
+        colored_header(
+            label=headline,
+            description=clear_text,
+            color_name="violet-70",
+        )
 
-                colored_header(
-                    label=headline,
-                    description=clear_text,
-                    color_name="violet-70",
-                )
-        except Exception as e:
-            st.error(f"An error occurred while processing: {e}")
+        # from streamlit_card import card
 
-        # Closing the file
-        audio_file.close()
-        # Removing the temporary file
-        os.remove(wav_file_path)
+        # card(
+        #     title=headline,
+        #     text=clear_text,
+        #     image="http://placekitten.com/300/250",
+        #     url="https://www.google.com",
+        # )
+
+        # Toggle to show the original transcription
+        # on = st.toggle("Original Transcription")
+        # if on:
+        #     st.write(transcript)
+
 
 if __name__ == "__main__":
     # Call the main function
